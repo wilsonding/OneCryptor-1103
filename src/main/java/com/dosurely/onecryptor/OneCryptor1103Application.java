d4cfe30f4f4f5a9bb9446e959c5a24fbafa9d7c6
Wilson Ding <wilsonding@Wilsons-MacBook-Pro.local>
2017-08-26 22:28:33 -0400

    Initialize the project

---
+++ src/main/java/com/dosurely/onecryptor/OneCryptor1103Application.java
@@ -0,0 +1,12 @@
+package com.dosurely.onecryptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCryptor1103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneCryptor1103Application.class, args);
+	}
+}
